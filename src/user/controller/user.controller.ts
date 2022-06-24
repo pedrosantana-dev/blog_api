72aa7e9ac7809fc8c3d12a08fc18fc39ffe55e6a
Pedro Santana <pedromanu27@gmail.com>
2022-06-24 18:14:34 -0300

    upload de imagem de perfil

--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -1,11 +1,29 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query, Request, Res, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { Pagination } from 'nestjs-typeorm-paginate';
-import { catchError, map, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of, tap } from 'rxjs';
 import { hasRoles } from 'src/auth/decorator/roles.decorator';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { User, UserRole } from '../models/user.interface';
 import { UserService } from '../service/user.service';
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
+import path = require('path');
+import { join } from 'path';
+
+export const storage = {
+    storage: diskStorage({
+        destination: './uploads/profileimages',
+        filename: (req, file, cb) => {
+            const filename: string = path.parse(file.originalname).name.replace(/\s/g, '') + uuidv4();
+            const extension: string = path.parse(file.originalname).ext;
+
+            cb(null, `${filename}${extension}`)
+        }
+    })
+}
+
 
 @Controller('users')
 export class UserController {
@@ -68,6 +86,23 @@ export class UserController {
     @Put(':id/role')
     updateRoleOfUser(@Param('id') id: string, @Body() user: User): Observable<User> {
         return this.userService.upadateRoleOfUser(Number(id), user);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Post('upload')
+    @UseInterceptors(FileInterceptor('file', storage))
+    uploadFile(@UploadedFile() file, @Request() req): Observable<Object> {
+        const user: User = req.user.user
+
+        return this.userService.updateOne(user.id, { profileImage: file.filename }).pipe(
+            tap((user: User) => console.log(user)),
+            map((user: User) => ({ profileImage: user.profileImage }))
+        )
+    }
+
+    @Get('profile-image/:imagename')
+    findProfileImage(@Param('imagename') imagename, @Res() res): Observable<Object> {
+        return of(res.sendFile(join(process.cwd(), 'uploads/profileimages/' + imagename)))
     }
 
 }
--- src/user/models/user.interface.ts
+++ src/user/models/user.interface.ts
@@ -13,4 +13,5 @@ export interface User {
     email?: string;
     password?: string;
     role?: UserRole;
+    profileImage?: string;
 }
